efbd00aef12c4fc9a4c0e4453bbad88c5c9ab2b4
Rajiv Nayan <rajivnayan@aesl.in>
2022-10-10 15:56:09 +0530

    initialize backend

---
+++ src/main/java/com/cultmart/culmartdemo/CulmartdemoApplication.java
@@ -0,0 +1,13 @@
+package com.cultmart.culmartdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulmartdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CulmartdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cultmart/culmartdemo/CulmartdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cultmart.culmartdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CulmartdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
